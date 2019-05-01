02f650fea0ca9c52b3173f0941167527f9a5846e
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-05-01 11:33:39 -0300

    Adding HomeComponent

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import "./App.scss";
 import { Container, Section } from 'rbx';
 import Header from "./components/header";
- 
+import Home from "./screens/home";
+
 const App = () => (
   <div>
     <Header />
+    <Home />
     <Section>
       <Container>
-        One Bit Food
       </Container>
     </Section>
   </div>
 )
- 
+
 export default App;
