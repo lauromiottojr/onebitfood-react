295a13dd977ccd90e984c506c1324abdc550afa3
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-05-05 16:21:23 -0300

    Configure Redux

---
+++ src/reducers/restaurantsReducer.js
@@ -0,0 +1,11 @@
+export default (state = { restaurants: [] }, action) => {
+    switch (action.type) {
+        case 'LOAD_RESTAURANTS':
+            return {
+                ...state,
+                restaurants: action.restaurants
+            }
+        default:
+            return state
+    }
+}
